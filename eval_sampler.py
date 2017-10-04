35284b4a387950a7fe5dab72802ba0a30843b1e1
Daniel Levy <daniel.levy0@gmail.com>
2017-10-04 02:59:35 +0000

    fix import

--- eval_sampler.py
+++ eval_sampler.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from utils.layers import Sequential, Zip, Parallel, Linear, ScaleTanh
 from utils.dynamics import Dynamics
 from utils.func_utils import get_data, binarize, tf_accept, autocovariance
-from utils.sampler import propose
+from utils.sampler import propose, chain_operator
 
 
 parser = argparse.ArgumentParser()
