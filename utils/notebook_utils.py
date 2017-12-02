87f3161c5b9ddf93ac2a17a74eea1bb10af75659
Daniel Levy <daniel.levy0@gmail.com>
2017-12-02 01:38:51 -0800

    added notebook_utils.py

---
+++ utils/notebook_utils.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import numpy as np
+from dynamics import Dynamics
+from sampler import propose
+import matplotlib.pyplot as plt
+
+def plot_grid(S, width=8):
+    sheet_width = width
+    plt.figure(figsize=(12, 12))
+    for i in xrange(S.shape[0]):
+        plt.subplot(sheet_width, sheet_width, i + 1)
+        plt.imshow(S[i], cmap='gray')
+        plt.grid('off')
+        plt.axis('off')
+        
+def plot_line(S):
+    sheet_width = S.shape[0]
+    plt.figure(figsize=(16, 3))
+    for i in xrange(S.shape[0]):
+        plt.subplot(1, sheet_width, i + 1)
+        plt.imshow(S[i], cmap='gray')
+        plt.grid('off')
+        plt.axis('off')
+        
+def get_hmc_samples(x_dim, eps, energy_function, sess, T=10, steps=200, samples=None):
+    hmc_dynamics = Dynamics(x_dim, energy_function, T=T, eps=eps, hmc=True)
+    hmc_x = tf.placeholder(tf.float32, shape=(None, x_dim))
+    Lx, _, px, hmc_MH = propose(hmc_x, hmc_dynamics, do_mh_step=True)
+    
+    if samples is None:
+        samples = gaussian.get_samples(n=200)
+        
+    final_samples = []
+    
+    for t in range(steps):
+        final_samples.append(np.copy(samples))
+        Lx_, px_, samples = sess.run([Lx, px, hmc_MH[0]], {hmc_x: samples})
+        
+    return np.array(final_samples)
